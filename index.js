39d95cea22f377dae3f6c5c8110088a7c5d2208a
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-02-02 14:28:55 +0100

    move docs from readme to js files

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
+/**
+ * @module admin-bro-expressjs
+ *
+ * @description
+ * Plugin which allows you to add AdminBro to expressjs applications.
+ *
+ * ## Installation
+ *
+ * ``````sh
+ * npm install admin-bro-expressjs
+ * ```
+ * 
+ * ## Usage
+ *
+ * ```
+ * const AdminBroExpress = require('admin-bro-expressjs')
+ * ```
+ *
+ * It exposes 2 methods which creates an Express Router which can be attached
+ * to given url in the API. Each method takes an preconfigured instance of {@link AdminBro}.
+ *
+ * - {@link module:admin-bro-expressjs.buildRouter AdminBroExpress.buildRouter(admin, [predefinedRouter])}
+ * - {@link module:admin-bro-expressjs.buildAuthenticatedRouter AdminBroExpress.buildAuthenticatedRouter(admin, auth, [predefinedRouter])}
+ *
+ * If you want to use a router you have already created - it is not a problem. Just pass it
+ * as a `predefinedRouter` parameter.
+ * 
+ * You may want to use this option when you want to include
+ * some custom auth middleware for you AdminBro routes.
+ *
+ * ## Example without an authentication
+ *
+ * ```
+ * const AdminBro = require('admin-bro')
+ * const AdminBroExpress = require('admin-bro-expressjs')
+ *
+ * const express = require('express')
+ * const app = express()
+ *
+ * const adminBro = new AdminBro({
+ *   databases: [],
+ *   rootPath: '/admin',
+ * })
+ *
+ * const router = AdminBroExpress.buildRouter(adminBro)
+ * app.use(adminBro.options.rootPath, router)
+ * app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
+ * ```
+ *
+ * ## Using build in authentication
+ *
+ * To protect the routes with an session authentication you can use predefined
+ * {@link module:admin-bro-expressjs.buildAuthenticatedRouter} method.
+ *
+ * ## Adding custom authentication
+ *
+ * Also you can add your custom authentication setup by first, creating the router, and then
+ * passing it via the `predefinedRouter` option.
+ *
+ * ```
+ * let router = express.Router()
+ * router.use((req, res, next) => {
+ *   if (req.session && req.session.admin) {
+ *     req.adminUser = req.session.admin
+ *     next()
+ *   } else {
+ *     res.redirect(adminBro.options.loginPath)
+ *   }
+ * })
+ * router = AdminBroExpress.buildRouter(adminBro, router)
+ * ```
+ */
+
 const Plugin = require('./plugin')
 
 module.exports = Plugin
--- plugin.js
+++ plugin.js
@@ -4,6 +4,8 @@ const AdminBro = require('admin-bro')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+const pkg = require('./package.json')
+
 let cookieParser
 let session
 
@@ -14,133 +16,165 @@ try {
   console.info('cookie-parser and express-session were not required')
 }
 
-const pkg = require('./package.json')
+/**
+ * Builds the Express Router handling all the pages and assets
+ *
+ * @param  {AdminBro} admin                       instance of adminBro
+ * @param  {express.Router} [predefinedRouter]    expressjs router
+ * @return {express.Router}                       expressjs router
+ * @function
+ * @static
+ * @memberof module:admin-bro-expressjs
+*/
+const buildRouter = (admin, predefinedRouter) => {
+  const { routes, assets } = AdminBro.Router
+  const router = predefinedRouter || express.Router()
 
-const Plugin = {
-  name: 'AdminBroExpressjs',
-  version: pkg.version,
+  router.use(bodyParser.json())
+  router.use(bodyParser.urlencoded({ extended: true }))
 
-  /**
-   * Builds the express router handling all the pages and assets
-   *
-   * @param  {AdminBro} adminBro                    instance of adminBro
-   * @param  {express.Router} [predefinedRouter]    expressjs router
-   * @return {express.Router}                       expressjs router
-  */
-  buildRouter: (admin, predefinedRouter) => {
-    const { routes, assets } = AdminBro.Router
-    const router = predefinedRouter || express.Router()
+  routes.forEach((route) => {
+    // we have to change routes defined in admin bro from {recordId} to :recordId
+    const expressPath = route.path.replace(/{/g, ':').replace(/}/g, '')
 
-    router.use(bodyParser.json())
-    router.use(bodyParser.urlencoded({ extended: true }))
-
-    routes.forEach((route) => {
-      // we have to change routes defined in admin bro from {recordId} to :recordId
-      const expressPath = route.path.replace(/{/g, ':').replace(/}/g, '')
-
-      const handler = async (req, res) => {
-        try {
-          const controller = new route.Controller({ admin }, req.adminUser)
-          const { params, query } = req
-          const payload = req.body
-          const html = await controller[route.action]({ params, query, payload }, res)
-          if (html) {
-            res.send(html)
-          }
-        } catch (e) {
-          // eslint-disable-next-line no-console
-          console.log(e)
+    const handler = async (req, res) => {
+      try {
+        const controller = new route.Controller({ admin }, req.adminUser)
+        const { params, query } = req
+        const payload = req.body
+        const html = await controller[route.action]({ params, query, payload }, res)
+        if (html) {
+          res.send(html)
         }
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
       }
-
-      if (route.method === 'GET') {
-        router.get(expressPath, handler)
-      }
-
-      if (route.method === 'POST') {
-        router.post(expressPath, handler)
-      }
-    })
-
-    assets.forEach((asset) => {
-      router.get(asset.path, async (req, res) => {
-        res.sendFile(path.resolve(asset.src))
-      })
-    })
-
-    return router
-  },
-
-  /**
-   * Builds the express router which requires authentication
-   *
-   * @param  {AdminBro} adminBro                    instance of adminBro
-   * @param  {Object} auth                          authenticatino options
-   * @param  {Function} auth.authenticate           function taking 2 arguments: email
-   *                                                and password. Returns authenticated
-   *                                                user or null in case of wrong email
-   *                                                and/or password
-   * @param  {String} auth.cookiePassword           secret used to encrypt cookies
-   * @param  {String} auth.cookieName=adminbro      cookie name
-   * @param  {express.Router} [predefinedRouter]    expressjs router
-   * @return {express.Router}                       expressjs router
-  */
-  buildAuthenticatedRouter(admin, auth, predefinedRouter) {
-    if (!cookieParser || !session) {
-      throw new Error(['In order to use authentication you have to install',
-        'cookie-parser and express-session packages'].join(' '))
     }
-    const router = predefinedRouter || express.Router()
-    router.use(cookieParser())
-    router.use(session({
-      secret: auth.cookiePassword,
-      name: auth.cookieName || 'adminbro',
-    }))
-    router.use(bodyParser.json())
-    router.use(bodyParser.urlencoded({ extended: true }))
 
-    const { rootPath } = admin.options
-    let { loginPath, logoutPath } = admin.options
-    loginPath = loginPath.replace(rootPath, '')
-    logoutPath = logoutPath.replace(rootPath, '')
+    if (route.method === 'GET') {
+      router.get(expressPath, handler)
+    }
 
-    router.get(loginPath, async (req, res) => {
-      const login = await AdminBro.renderLogin({ action: admin.options.loginPath })
-      res.send(login)
+    if (route.method === 'POST') {
+      router.post(expressPath, handler)
+    }
+  })
+
+  assets.forEach((asset) => {
+    router.get(asset.path, async (req, res) => {
+      res.sendFile(path.resolve(asset.src))
     })
+  })
 
-    router.post(loginPath, async (req, res) => {
-      const { email, password } = req.body
-      const adminUser = await auth.authenticate(email, password)
-      if (adminUser) {
-        req.session.adminUser = adminUser
-        res.redirect(rootPath)
-      } else {
-        const login = await AdminBro.renderLogin({
-          action: admin.options.loginPath,
-          errorMessage: 'Invalid credentials!',
-        })
-        res.send(login)
-      }
-    })
-
-    router.use((req, res, next) => {
-      if (AdminBro.Router.assets.find(asset => req.originalUrl.match(asset.path))) {
-        next()
-      } else if (req.session.adminUser) {
-        next()
-      } else {
-        res.redirect(admin.options.loginPath)
-      }
-    })
-
-    router.get(logoutPath, async (req, res) => {
-      req.session.destroy()
-      res.redirect(admin.options.loginPath)
-    })
-
-    return Plugin.buildRouter(admin, router)
-  },
+  return router
 }
 
-module.exports = Plugin
+/**
+ * Builds the Express Router which is protected by session auth
+ *
+ * Using the ruter requires you to install both `express-session` and `cookie-parser` as a
+ * dependency.
+ *
+ * @param  {AdminBro} admin                    instance of adminBro
+ * @param  {Object} auth                          authenticatino options
+ * @param  {Function} auth.authenticate           function taking 2 arguments: email
+ *                                                and password. Returns authenticated
+ *                                                user or null in case of wrong email
+ *                                                and/or password
+ * @param  {String} auth.cookiePassword           secret used to encrypt cookies
+ * @param  {String} auth.cookieName=adminbro      cookie name
+ * @param  {express.Router} [predefinedRouter]    expressjs router
+ * @return {express.Router}                       expressjs router
+ * @static
+ * @memberof module:admin-bro-expressjs
+ * @example
+ * const ADMIN = {
+ *   email: 'test@example.com',
+ *   password: 'password',
+ * }
+ *
+ * AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+ *   authenticate: async (email, password) => {
+ *     if (ADMIN.password === password && ADMIN.email === email) {
+ *       return ADMIN
+ *     }
+ *     return null
+ *   },
+ *   cookieName: 'adminbro',
+ *   cookiePassword: 'somepassword',
+ * }, [router])
+*/
+const buildAuthenticatedRouter = (admin, auth, predefinedRouter) => {
+  if (!cookieParser || !session) {
+    throw new Error(['In order to use authentication you have to install',
+      'cookie-parser and express-session packages'].join(' '))
+  }
+  const router = predefinedRouter || express.Router()
+  router.use(cookieParser())
+  router.use(session({
+    secret: auth.cookiePassword,
+    name: auth.cookieName || 'adminbro',
+  }))
+  router.use(bodyParser.json())
+  router.use(bodyParser.urlencoded({ extended: true }))
+
+  const { rootPath } = admin.options
+  let { loginPath, logoutPath } = admin.options
+  loginPath = loginPath.replace(rootPath, '')
+  logoutPath = logoutPath.replace(rootPath, '')
+
+  router.get(loginPath, async (req, res) => {
+    const login = await AdminBro.renderLogin({ action: admin.options.loginPath })
+    res.send(login)
+  })
+
+  router.post(loginPath, async (req, res) => {
+    const { email, password } = req.body
+    const adminUser = await auth.authenticate(email, password)
+    if (adminUser) {
+      req.session.adminUser = adminUser
+      res.redirect(rootPath)
+    } else {
+      const login = await AdminBro.renderLogin({
+        action: admin.options.loginPath,
+        errorMessage: 'Invalid credentials!',
+      })
+      res.send(login)
+    }
+  })
+
+  router.use((req, res, next) => {
+    if (AdminBro.Router.assets.find(asset => req.originalUrl.match(asset.path))) {
+      next()
+    } else if (req.session.adminUser) {
+      next()
+    } else {
+      res.redirect(admin.options.loginPath)
+    }
+  })
+
+  router.get(logoutPath, async (req, res) => {
+    req.session.destroy()
+    res.redirect(admin.options.loginPath)
+  })
+
+  return buildRouter(admin, router)
+}
+
+module.exports = {
+  buildRouter,
+  buildAuthenticatedRouter,
+  /**
+   * Version of the plugin
+   * @static
+   * @memberof module:admin-bro-expressjs
+  */
+  version: pkg.version,
+  /**
+   * Plugin name
+   * @static
+   * @memberof module:admin-bro-expressjs
+  */
+  name: 'AdminBroExpressjs',
+}
