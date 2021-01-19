e09e304131ea48770d86394b86447a40c82f9d56
JonaszJestem <jonaszwiacek@gmail.com>
2021-01-19 14:05:58 +0100

    fix: Do not protect non-admin routes

--- src/authentication/protected-routes.handler.ts
+++ src/authentication/protected-routes.handler.ts
@@ -8,7 +8,7 @@ export const withProtectedRoutesHandler = (
   const { rootPath } = admin.options;
 
   router.use((req, res, next) => {
-    if (AdminRouter.assets.find((asset) => req.originalUrl.match(asset.path))) {
+    if (isAdminAsset(req.originalUrl)) {
       next();
     } else if (
       req.session.adminUser ||
@@ -17,18 +17,31 @@ export const withProtectedRoutesHandler = (
       req.originalUrl.startsWith(admin.options.logoutPath)
     ) {
       next();
-    } else {
+    } else if (isAdminRoute(req.originalUrl, rootPath)) {
       // If the redirection is caused by API call to some action just redirect to resource
       const [redirectTo] = req.originalUrl.split("/actions");
       req.session.redirectTo = redirectTo.includes(`${rootPath}/api`)
         ? rootPath
         : redirectTo;
+
       req.session.save((err) => {
         if (err) {
           next(err);
         }
         res.redirect(admin.options.loginPath);
       });
+    } else {
+      next();
     }
   });
 };
+
+const isAdminRoute = (url: string, adminRootUrl: string) => {
+  const adminRoutes = AdminRouter.routes.filter((route) => route.path !== "");
+  const isAdminRootUrl = url === adminRootUrl;
+
+  return isAdminRootUrl || adminRoutes.find((route) => route.path === url);
+};
+
+const isAdminAsset = (url: string) =>
+  AdminRouter.assets.find((asset) => url.match(asset.path));
